3d208be24a4ea402ee1b5d8db946ca83eb01a88c
Alexei <uhamae@gmail.com>
2022-10-08 22:50:18 +0300

    Frontend initizliation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+      {/* <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      {/* <View> */}
+      <View>
+        <Image source={require('./img/chocolate.png')} 
+          style={styles.logo}
+        />
+        <Text>Splitka</Text>
+      </View>
+      
+      {/* </View> */}
+      </SafeAreaView>
   );
 }
 
@@ -14,7 +27,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  logo: { 
+    width: 40,
+    height: 40,
+    marginLeft: 24,
+    marginTop: 57,
+  }
 });
