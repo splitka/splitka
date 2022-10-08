37968d0dab3159876bf38f7dc00f8fb2002c74d8
Захар Назаров <s02190158@live.cs.msu.ru>
2022-10-08 22:57:31 +0300

    delete garbage and add auth model

--- backend/endpoints/auth.py
+++ backend/endpoints/auth.py
@@ -1,17 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.token import Login
 import requests, json, base64
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import aiohttp
 
 url_TOKEN = "https://auth.bankingapi.ru/auth/realms/kubernetes/protocol/openid-connect/token"
 url_me = "https://hackaton.bankingapi.ru/api/vtbid/v1/oauth2/me?scope=mainMobilePhone"
 
 router = APIRouter()
+security = HTTPBasic()
 
-@router.post("/")
-async def login(login: Login):
+@router.get("/")
+async def login(credentials: HTTPBasicCredentials = Depends(security)):
   # Получаем token 
-  data_Authorization = base64.b64encode(bytes(f"{login.username}:{login.password}", encoding = "utf-8"))
-  data_Authorization = 'Basic '+str(data_Authorization.decode('UTF-8'))
+  # print(credentials.username, flush=True)
+  # print(credentials.password, flush=True)
+  data_Authorization = base64.b64encode(bytes(f"{credentials.username}:{credentials.password}", encoding = "utf-8"))
+  data_Authorization = 'Basic ' + str(data_Authorization.decode('UTF-8'))
   headers = {
   'Authorization': data_Authorization,
   'Content-Type': 'application/x-www-form-urlencoded'
@@ -19,13 +25,18 @@ async def login(login: Login):
 
   payload='grant_type=client_credentials'
   response = requests.request("POST", url_TOKEN, headers=headers, data=payload)
+  
+  async with aiohttp.ClientSession() as session:
+    async with session.post(url_TOKEN, headers=headers, data=payload) as resp:
+      response = await resp.json()
 
-  response_dict = json.loads(response.text)
-  access_token = response_dict["access_token"]
+  # response_dict = json.loads(response.text)
+  # access_token = response_dict["access_token"]
+  access_token = response["access_token"]
   # делаем запрос в /me
   payload={}
   headers = {
-    'Authorization': 'Bearer '+ access_token
+    'Authorization': 'Bearer ' + access_token
   }
 
   response = requests.request("GET", url_me, headers=headers, data=payload)
