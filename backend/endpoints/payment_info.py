9f359fa3a3d675e02add8690055509b6bc0acca4
Nazacod <s02190158@gse.cs.msu.ru>
2022-10-09 17:00:58 +0300

    fix models

--- backend/endpoints/payment_info.py
+++ backend/endpoints/payment_info.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from models.models import Token, TransactionResponse
+from models.models import Token, TransactionResponse, KeyNotAuthorized
 import requests, json, base64, aiohttp
+from typing import Union
 
 router = APIRouter()
 import requests
@@ -8,7 +9,7 @@ import requests
 url_payments = "https://hackaton.bankingapi.ru/api/rb/pmnt/acceptance/universal/hackathon/v1/payments/0"
 
 # TODO: change puk and 2
-@router.post("/", response_model=TransactionResponse)
+@router.post("/", response_model=Union[TransactionResponse, KeyNotAuthorized])
 async def payment_info(login: Token):
     # defolt headers expect Authorization
     payload={}
@@ -27,7 +28,8 @@ async def payment_info(login: Token):
     async with aiohttp.ClientSession() as session:
         async with session.get(url_payments, headers=headers, data=payload, skip_auto_headers=['Content-Type']) as resp:
             response = await resp.json()
-
+    
+    print(response, flush=True)
     return response
 
 
--- backend/models/models.py
+++ backend/models/models.py
@@ -1,15 +1,19 @@
 from re import L
 from pydantic import BaseModel
+from typing import Union
 
 class Login(BaseModel):
     username: str
     password: str
 
+
 class Token(BaseModel):
     access_token: str
 
+
 class MyAddress(BaseModel):
     addressName: str
+
 
 class Passport(BaseModel):
     series: int
@@ -25,24 +29,26 @@ class Passport(BaseModel):
     issuedBy: str
     issueId: str
 
+
 class BasicInfo(BaseModel):
-  surname: str
-  name: str
-  patronymic: str
-  gender: str
-  birthPlace: str
-  birthDate: str
-  maritalStatus: str
-  mainMobilePhone: str
-  mobilePhone: str
-  email: str
-  registrationAddress: MyAddress
-  temporaryAddress: MyAddress
-  actualAddress: MyAddress
-  snils: str
-  inn: int
-  rfPassport: Passport
-  mdmId: int
+    surname: str
+    name: str
+    patronymic: str
+    gender: str
+    birthPlace: str
+    birthDate: str
+    maritalStatus: str
+    mainMobilePhone: str
+    mobilePhone: str
+    email: str
+    registrationAddress: MyAddress
+    temporaryAddress: MyAddress
+    actualAddress: MyAddress
+    snils: str
+    inn: int
+    rfPassport: Passport
+    mdmId: int
+
 
 class Actions(BaseModel):
     autoPaymentByBalance: bool
@@ -52,16 +58,20 @@ class Actions(BaseModel):
     repeat: bool
     template: bool
 
+
 class ProductType(BaseModel):
     productType: str
     publicId: str
 
+
 class InfoCode(BaseModel):
     code: str
+
 
 class InfoSum(BaseModel):
     amount: int
     currency: InfoCode
+
 
 class InfoFiels(BaseModel):
     description: str
@@ -70,9 +80,11 @@ class InfoFiels(BaseModel):
     type: str
     value: int
 
+
 class InfoStatus(BaseModel):
     code: str
     description: str
+
 
 class InfoProvider(BaseModel):
     categoryIds: list[str]
@@ -81,9 +93,13 @@ class InfoProvider(BaseModel):
     subTypes: list[str]
     type: str
 
+class InfoClientProduct(BaseModel):
+    account: ProductType
+    card: ProductType
+
 class TransactionResponse(BaseModel):
     actions: Actions
-    clientProduct: list[ProductType]
+    clientProduct: InfoClientProduct
     commissionSum: InfoSum
     createdAt: str
     description: str
@@ -95,8 +111,88 @@ class TransactionResponse(BaseModel):
     status: InfoStatus
     totalSum: InfoSum
 
+
 class TokensResponse(BaseModel):
     scope: str
     access_token: str
     refresh_token: str
     id_token: str
+
+
+class KeyNotAuthorized(BaseModel):
+    error: str
+
+
+# {
+#     'actions':
+#     {
+#         'autoPaymentByBalance': True,
+#         'autoPaymentByDate': True,
+#         'check': True,
+#         'remind': True,
+#         'repeat': True,
+#         'template': True
+#     },
+#     'clientProduct':
+#     {
+#         'account':
+#         {
+#             'productType': 'CARD',
+#             'publicId': 'string'
+#         },
+#         'card':
+#         {
+#             'productType': 'ACCOUNT',
+#             'publicId': 'string'
+#         }
+#     },
+#     'commissionSum':
+#     {
+#         'amount': 600,
+#         'currency':
+#         {
+#             'code': 'RUB'
+#         }
+#     },
+#     'createdAt': '2019-08-24T14:15:22Z',
+#     'description': 'string',
+#     'documentId': '1234567890',
+#     'fields': [
+#         {
+#             'description': 'Лицевой счет',
+#             'key': 'phone',
+#             'name': 'Лицевой счет',
+#             'type': 'STRING',
+#             'value': '89535184636'
+#         }],
+#     'id': '4d9065bc-55f7-4317-b728-5097344dc72f',
+#     'paySum':
+#     {
+#         'amount': 600,
+#         'currency':
+#         {
+#             'code': 'RUB'
+#         }
+#     },
+#     'providerService':
+#     {
+#         'categoryIds': ['string'],
+#         'id': '1',
+#         'name': 'КИВИ',
+#         'subTypes': ['MOBILE_BY_PHONE'],
+#         'type': 'CONTRACT'
+#     },
+#     'status':
+#     {
+#         'code': 'PROCESSING',
+#         'description': 'string'
+#     },
+#     'totalSum':
+#     {
+#         'amount': 600,
+#         'currency':
+#         {
+#             'code': 'RUB'
+#         }
+#     }
+# }
